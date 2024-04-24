c06706ff7f9ef638d5ca9de5c700168e805818b8
Markus Böck <markus.boeck02@gmail.com>
2024-04-24 11:28:19 +0100

    [runtime] Perform basic device and hal module creation

--- runtime/runtime/src/Quidditch/device.c
+++ runtime/runtime/src/Quidditch/device.c
@@ -1,10 +1,82 @@
 #include "device.h"
 
+#include <iree/hal/local/local_executable_cache.h>
+
+typedef struct quidditch_device_t {
+  iree_hal_resource_t resource;
+
+  iree_allocator_t host_allocator;
+  iree_hal_allocator_t *device_allocator;
+
+  iree_host_size_t loader_count;
+  iree_hal_executable_loader_t *loaders[];
+} quidditch_device_t;
+
+static const iree_hal_device_vtable_t quidditch_device_vtable;
+
+static quidditch_device_t *cast_device(iree_hal_device_t *device) {
+  IREE_HAL_ASSERT_TYPE(device, &quidditch_device_vtable);
+  return (quidditch_device_t *)device;
+}
+
+static void destroy(iree_hal_device_t *base_device) {
+  quidditch_device_t *device = cast_device(base_device);
+  iree_hal_allocator_release(device->device_allocator);
+  for (iree_host_size_t i = 0; i < device->loader_count; i++) {
+    iree_hal_executable_loader_release(device->loaders[i]);
+  }
+}
+
+static iree_allocator_t host_allocator(iree_hal_device_t *base_device) {
+  return cast_device(base_device)->host_allocator;
+}
+
+static iree_status_t create_executable_cache(
+    iree_hal_device_t *base_device, iree_string_view_t identifier,
+    iree_loop_t loop, iree_hal_executable_cache_t **out_executable_cache) {
+  quidditch_device_t *device = cast_device(base_device);
+  return iree_hal_local_executable_cache_create(
+      identifier, /*worker_capacity=*/1, device->loader_count, device->loaders,
+      iree_hal_device_host_allocator(base_device), out_executable_cache);
+}
+
+static const iree_hal_device_vtable_t quidditch_device_vtable = {
+    .destroy = destroy,
+    .host_allocator = host_allocator,
+    .create_executable_cache = create_executable_cache,
+};
+
 iree_status_t quidditch_device_create(iree_host_size_t loader_count,
                                       iree_hal_executable_loader_t **loaders,
                                       iree_hal_allocator_t *device_allocator,
                                       iree_allocator_t host_allocator,
                                       iree_hal_device_t **out_device) {
-  // TODO: Actual device creation...
-  return iree_make_status(IREE_STATUS_UNIMPLEMENTED);
+  IREE_ASSERT_ARGUMENT(loaders || loader_count == 0);
+  IREE_ASSERT_ARGUMENT(device_allocator);
+  IREE_ASSERT_ARGUMENT(out_device);
+
+  // Set the out device to null in case any steps fail.
+  *out_device = NULL;
+
+  quidditch_device_t *device = NULL;
+  iree_host_size_t allocation_size =
+      sizeof(quidditch_device_t) +
+      loader_count * sizeof(*device->loaders);  // NOLINT(*-sizeof-expression)
+  IREE_RETURN_IF_ERROR(
+      iree_allocator_malloc(host_allocator, allocation_size, (void **)&device));
+  memset(device, 0, allocation_size);
+  iree_hal_resource_initialize(&quidditch_device_vtable, &device->resource);
+  device->host_allocator = host_allocator;
+  device->device_allocator = device_allocator;
+
+  // Make sure to increase the ref counts of any entities we reference.
+  iree_hal_allocator_retain(device_allocator);
+  device->loader_count = loader_count;
+  for (iree_host_size_t i = 0; i < device->loader_count; ++i) {
+    device->loaders[i] = loaders[i];
+    iree_hal_executable_loader_retain(device->loaders[i]);
+  }
+
+  *out_device = (iree_hal_device_t *)device;
+  return iree_ok_status();
 }
--- runtime/samples/main.c
+++ runtime/samples/main.c
@@ -3,46 +3,98 @@
 #include <iree/base/allocator.h>
 #include <iree/hal/allocator.h>
 #include <iree/hal/local/loaders/static_library_loader.h>
+#include <iree/modules/hal/module.h>
 #include <iree/modules/hal/types.h>
 #include <iree/vm/instance.h>
 
 #include <team_decls.h>
 
-static iree_status_t setup() {
-  iree_allocator_t host_allocator = iree_allocator_system();
+static iree_status_t setup_instance_and_device(
+    iree_allocator_t host_allocator, iree_vm_instance_t** out_instance,
+    iree_hal_device_t** out_device) {
+  IREE_ASSERT_ARGUMENT(out_instance);
+  IREE_ASSERT_ARGUMENT(out_device);
 
-  iree_vm_instance_t *instance;
   IREE_RETURN_IF_ERROR(iree_vm_instance_create(IREE_VM_TYPE_CAPACITY_DEFAULT,
-                                               host_allocator, &instance));
+                                               host_allocator, out_instance));
 
-  IREE_RETURN_IF_ERROR(iree_hal_module_register_all_types(instance));
+  iree_status_t result = iree_hal_module_register_all_types(*out_instance);
+  if (!iree_status_is_ok(result)) goto error_release_vm;
 
   const iree_hal_executable_library_query_fn_t libraries[] = {};
 
-  iree_hal_executable_loader_t *loader;
-  IREE_RETURN_IF_ERROR(iree_hal_static_library_loader_create(
+  iree_hal_executable_loader_t* loader;
+  result = iree_hal_static_library_loader_create(
       IREE_ARRAYSIZE(libraries), libraries,
-      iree_hal_executable_import_provider_null(), host_allocator, &loader));
+      iree_hal_executable_import_provider_null(), host_allocator, &loader);
+  if (!iree_status_is_ok(result)) goto error_release_vm;
 
   // TODO: Replace with more sophisticated allocator representing cluster
   // memory.
-  iree_hal_allocator_t *device_allocator;
-  IREE_RETURN_IF_ERROR(iree_hal_allocator_create_heap(
-      iree_make_cstring_view("quidditch"), host_allocator, host_allocator,
-      &device_allocator));
+  iree_hal_allocator_t* device_allocator;
+  result = iree_hal_allocator_create_heap(iree_make_cstring_view("quidditch"),
+                                          host_allocator, host_allocator,
+                                          &device_allocator);
+  if (!iree_status_is_ok(result)) goto error_release_library_loader;
 
-  iree_hal_device_t *device;
-  IREE_RETURN_IF_ERROR(quidditch_device_create(
-      /*loader_count=*/1, &loader, device_allocator, host_allocator, &device))
+  result = quidditch_device_create(
+      /*loader_count=*/1, &loader, device_allocator, host_allocator,
+      out_device);
+  iree_hal_executable_loader_release(loader);
+  iree_hal_allocator_release(device_allocator);
+  return result;
 
-  return iree_ok_status();
+error_release_library_loader:
+  iree_hal_executable_loader_release(loader);
+error_release_vm:
+  iree_vm_instance_release(*out_instance);
+  return result;
 }
 
 int main() {
   // TODO: Remove/redirect compute cores once implemented.
   if (!snrt_is_dm_core()) return 0;
 
-  iree_status_t result = setup();
+  iree_allocator_t host_allocator = iree_allocator_system();
+
+  iree_vm_instance_t* vmInstance;
+  iree_hal_device_t* device;
+  iree_status_t result =
+      setup_instance_and_device(host_allocator, &vmInstance, &device);
+  if (!iree_status_is_ok(result)) {
+    iree_status_fprint(stderr, result);
+    iree_status_free(result);
+    return -1;
+  }
+
+  // TODO: Create EmitC module here.
+
+  iree_vm_module_t* hal_module = NULL;
+  result =
+      iree_hal_module_create(vmInstance, /*device_count=*/1,
+                             /*devices=*/&device, IREE_HAL_MODULE_FLAG_NONE,
+                             host_allocator, &hal_module);
+  if (!iree_status_is_ok(result)) goto error_release_instance_and_device;
+
+  iree_vm_module_t* modules[] = {hal_module};
+
+  iree_vm_context_t* context;
+  result = iree_vm_context_create_with_modules(
+      vmInstance, IREE_VM_CONTEXT_FLAG_NONE, IREE_ARRAYSIZE(modules), modules,
+      host_allocator, &context);
+  if (!iree_status_is_ok(result)) goto error_release_hal_module;
+
+  // TODO: Run modules.
+
+  iree_vm_context_release(context);
+error_release_hal_module:
+  iree_vm_module_release(hal_module);
+
+error_release_instance_and_device:
+  iree_hal_device_release(device);
+  iree_vm_instance_release(vmInstance);
+
+exit:
   if (!iree_status_is_ok(result)) {
     iree_status_fprint(stderr, result);
     iree_status_free(result);
