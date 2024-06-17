564a0c8aab0432b8ceadfdb1432700c88227a127
Markus Böck <markus.boeck02@gmail.com>
2024-06-17 16:40:48 +0100

    [compiler] Add pattern to raise ReLUs in select to `arith.maximumf` form (#33)
    The latter xDSL is capable of lowering to floating point instructions
    that can be placed inside a `frep` and is therefore a lot faster.
    
    As a side-effect two more kernels in NsNet2 can be compiled which showed
    an issue with `hal.executable.condition` in the linker that had to be
    fixed as well.

---
+++ codegen/compiler/src/Quidditch/ReluToMax.cpp
@@ -0,0 +1,77 @@
+#include "Passes.h"
+
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+namespace quidditch {
+#define GEN_PASS_DEF_RELUTOMAXPASS
+#include "Quidditch/Passes.h.inc"
+} // namespace quidditch
+
+using namespace mlir;
+
+namespace {
+class ReluToMax : public quidditch::impl::ReluToMaxPassBase<ReluToMax> {
+public:
+  using Base::Base;
+
+protected:
+  void runOnOperation() override;
+};
+
+// Apparently arith.cmpf do not have a canonical representation for either LT
+// or GT.
+
+struct ReluSelectToMaxLTPattern : OpRewritePattern<arith::SelectOp> {
+
+  using OpRewritePattern<arith::SelectOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(arith::SelectOp op,
+                                PatternRewriter &rewriter) const override {
+    auto cmpFOp = op.getCondition().getDefiningOp<arith::CmpFOp>();
+    if (!cmpFOp)
+      return failure();
+    if (cmpFOp.getPredicate() != arith::CmpFPredicate::ULT)
+      return failure();
+    if (cmpFOp.getRhs() != op.getTrueValue())
+      return failure();
+    if (cmpFOp.getLhs() != op.getFalseValue())
+      return failure();
+
+    rewriter.replaceOpWithNewOp<arith::MaximumFOp>(op, cmpFOp.getLhs(),
+                                                   cmpFOp.getRhs());
+    return success();
+  }
+};
+
+struct ReluSelectToMaxGTPattern : OpRewritePattern<arith::SelectOp> {
+
+  using OpRewritePattern<arith::SelectOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(arith::SelectOp op,
+                                PatternRewriter &rewriter) const override {
+    auto cmpFOp = op.getCondition().getDefiningOp<arith::CmpFOp>();
+    if (!cmpFOp)
+      return failure();
+    if (cmpFOp.getPredicate() != arith::CmpFPredicate::UGT)
+      return failure();
+    if (cmpFOp.getRhs() != op.getFalseValue())
+      return failure();
+    if (cmpFOp.getLhs() != op.getTrueValue())
+      return failure();
+
+    rewriter.replaceOpWithNewOp<arith::MaximumFOp>(op, cmpFOp.getLhs(),
+                                                   cmpFOp.getRhs());
+    return success();
+  }
+};
+
+} // namespace
+
+void ReluToMax::runOnOperation() {
+  RewritePatternSet patterns(&getContext());
+  patterns.insert<ReluSelectToMaxLTPattern, ReluSelectToMaxGTPattern>(
+      &getContext());
+  if (failed(applyPatternsAndFoldGreedily(getOperation(), std::move(patterns))))
+    signalPassFailure();
+}
